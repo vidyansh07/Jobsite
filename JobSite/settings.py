a44b688a77a3113b45ceedde6a792ad373800742
vidyansh07 <vidyansht@gmail.com>
2025-03-25 23:37:39 +0530

    Adding the updated files

--- JobSite/settings.py
+++ JobSite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,18 +89,12 @@ WSGI_APPLICATION = 'JobSite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #  use sqlite3
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobdatabase',
-        'USER': 'root',
-        'PASSWORD': 'agtma',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
